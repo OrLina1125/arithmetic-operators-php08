3b21f37e7ba25d33924a71d9c2442aa6672dba47
Orlina M Brabar <90068655+OrLina1125@users.noreply.github.com>
2022-04-09 11:33:34 +0900

    add Aritmetic operators

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the document</title>
+</head>
+<body>
+
+
+<?php
+    // arithmetic Operators
+
+    // echo 5+5;
+    // echo 5-5;
+    // echo 5/5;
+    // echo 5*5;
+    // echo 8%5;
+    echo 5**2;
+
+?>
+</body>
+</html>
